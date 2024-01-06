86cb315f7ec92d74f2e83496666565071e479fb2
Nour Meriem Bouayed <bouayed.nour@gmail.com>
2024-01-06 17:40:39 +0100

    testing TSFEL features

--- custom_code/add_custom_feature.py
+++ custom_code/add_custom_feature.py
@@ -1,6 +1,9 @@
-from tsfel.utils.add_personal_features import add_feature_json
 import os
+os.chdir('..')
 
-add_feature_json("custom_code/custom_feature.py","custom_code/features.json")
+from tsfel.utils.add_personal_features import add_feature_json
+
+add_feature_json(r"C:/Users/TRETEC/Documents/MVA/S1/[AST]/TSFEL_Review/custom_code/custom_feature.py",r"C:/Users/TRETEC/Documents/MVA/S1/[AST]/TSFEL_Review/custom_code/features.json")
+
 
 
